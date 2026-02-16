3904d0f6a6df3155774e936ddee5d5fecb3f51b3
Luis Angel Hernandez Garcia <lahernandezg@gomsa.com>
2026-02-16 11:42:08 -0600

    nueva carpeta

---
+++ Operaciones Aritmeticas/nuevoPrograma.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("nuevo programa");
+Console.WriteLine("de prueba");
+
+int resultado = 2 + 2;
+Console.WriteLine($"El resultado es: {resultado}");
--- nuevoPrograma.cs
+++
@@ -1,2 +0,0 @@
-Console.WriteLine("nuevo programa");
-Console.WriteLine("de prueba");
